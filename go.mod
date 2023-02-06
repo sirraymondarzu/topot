b38e933c1b9c60ddcdf6648429f5e6606d0cb5a5
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-02-06 13:23:14 +1100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module arzuraymond.net/topot
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hi there! We are tapot."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	//create a file server
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	//create url mapping to the static directory
+	//mux.Handle("/resource/", http.StripPrefix("/resource", fileServer))
+	mux.Handle("/resource/", http.StripPrefix("/resource/", fileServer))
+	log.Println("Starting on port :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
