f0785e09bca4b2d0c8916c18dc0221acc08dfaad
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-02-06 17:05:35 +1100

    creating forms

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,24 +13,36 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func displayTime(w http.ResponseWriter, r *http.Request) {
 	localtime := time.Now().Format("3:04:05 PM")
-	
 	//read in the template file
 	ts, _ := template.ParseFiles("./ui/html/display.time.tmpl")
 	ts.Execute(w, localtime)
 
 }
+func getValues(w http.ResponseWriter, r *http.Request) {
+	
+
+}
+func calculateArea(w http.ResponseWriter, r *http.Request) {
+	
+}
 
 func main() {
 	mux := http.NewServeMux()
+
+	//register routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/time", displayTime)
+	mux.HandleFunc("/area-calculator", getValues)
+	mux.HandleFunc("/area-calculator-2", calculateArea)
+
+
 
 	//create a file server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	//create url mapping to the static directory
 
-	mux.Handle("/resource/", http.StripPrefix("/resource/", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	log.Println("Starting on port :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
